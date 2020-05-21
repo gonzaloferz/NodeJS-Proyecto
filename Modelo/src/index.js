de3cf478b5b5645e6b5eb15d9a4f8e8f39210cae
gmf92 <gmf92@hotmail.com>
2020-05-21 13:37:55 -0300

    .

--- Modelo/src/index.js
+++ Modelo/src/index.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+
 
 //Settings
-app.set('port', 3000)
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
 
 
 //Middlewares
 
 
 //Routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
+app.use(require('./routes/'));
 
 
 //Static files
-
 
 
 //Listening the server
