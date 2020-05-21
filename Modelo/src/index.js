d1e304f6a7c3c22d83a8bb613be24d998125d480
gmf92 <gmf92@hotmail.com>
2020-05-21 14:39:26 -0300

    Agregamos template

--- Modelo/src/index.js
+++ Modelo/src/index.js
@@ -6,13 +6,15 @@ const path = require('path');
 //Settings
 app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile); 
+app.set('view engine', 'ejs');
 
 
 //Middlewares
 
 
 //Routes
-app.use(require('./routes/'));
+app.use(require('./routes/index'));
 
 
 //Static files
--- Modelo/src/routes/index.js
+++ Modelo/src/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
+    res.render('index.html');
 });
+
+
+router.get('/contacto', (req, res) => {
+    res.render('contacto.html');
+});
+
 
 module.exports = router;
