f9fb28124731794ead33490a837fe10e1b8cc03d
gmf92 <gmf92@hotmail.com>
2020-05-21 11:58:40 -0300

    Creamos servidor

--- Modelo/src/index.js
+++ Modelo/src/index.js
@@ -1,1 +1,25 @@
-console.log('Server funcionando!');
+const express = require('express');
+const app = express();
+const path = require('path')
+
+//Settings
+app.set('port', 3000)
+
+
+//Middlewares
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+
+//Static files
+
+
+
+//Listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
