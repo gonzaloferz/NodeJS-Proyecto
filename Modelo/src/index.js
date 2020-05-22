933711555e0233f83786894fbf8c7be902273fab
gmf92 <gmf92@hotmail.com>
2020-05-22 14:51:31 -0300

    Agregamos mas metodos

--- Modelo/src/index.js
+++ Modelo/src/index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+//const morgan = require('morgan');
 
 
 //Settings
-app.set('port', 3000);
+//setea el puerto en la posicion indicada o donde el servicio de la nuve indique 
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile); 
 app.set('view engine', 'ejs');
+//Acomoda el formato Json
+app.set('json spaces', 2);
 
 
 //Middlewares
+//Da informacion al servidor sobre algun error
+//app.use(morgan('dev'));
+//Permite al servidor entender los datos que llegue de formularios
+app.use(express.urlencoded({extended: false}));
+//Permite al servidor entender los formatos Json
+app.use(express.json());
 
 
 //Routes
 app.use(require('./routes/index'));
+app.use('/api/movies', require('./routes/movies'));
 
 
 //Static files
