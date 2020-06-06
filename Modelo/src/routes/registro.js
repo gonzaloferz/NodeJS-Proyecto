4e0724ce5c4c82a7644d549b8d34a72e11a2616e
gmf92 <gmf92@hotmail.com>
2020-06-06 18:04:41 -0300

    Update registro.js

--- Modelo/src/routes/registro.js
+++ Modelo/src/routes/registro.js
@@ -2,13 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 
-/* //Validar registro
+//Validar registro
 router.post('/', (req, res) => {
     const { pais, nombre, apellido, dia, mes, año, email, contraseña } = req.body;
 
-    if ( typeof nombre  === 'string' && typeof apellido === 'string' && /^[a-z]+$/i.test(email)){
-        
+    if ( typeof nombre  !== 'string'){
+        throw new Error('Tipo invalido')
     }
-}) */
+    
+    if ( typeof apellido !== 'string'){
+        throw new Error('Tipo invalido')
+    }
+    
+    if ( typeof dia !== 'number'){
+        throw new Error('Tipo invalido')
+    }
+    
+    if ( typeof año !== 'number'){
+        throw new Error('Tipo invalido')
+    }
+    
+})
 
 module.exports = router;
